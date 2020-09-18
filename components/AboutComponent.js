20eb3da47a38f359af734ced0eff7a7c59176566
masterdrue <masterdrue@gmail.com>
2020-09-17 22:58:10 -0400

    Redux Store

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 import { Text, ScrollView } from 'react-native';
 import { FlatList } from 'react-native';
 import { ListItem, Card } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
 
 function Mission() {
     return(
@@ -21,13 +28,6 @@ function Mission() {
 
 class About extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        };
-    }
-
     static navigationOptions = {
         title: 'About Us'
     }
@@ -38,7 +38,7 @@ class About extends Component {
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{ source: {uri: baseUrl + item.image}}}
                 /> 
             );
         };    
@@ -50,7 +50,7 @@ class About extends Component {
                     </Card>
                     <Card title="Community Partners">
                         <FlatList
-                            data={this.state.partners}
+                            data={this.props.partners.partners}
                             renderItem={renderPartner}
                             keyExtractor={item => item.id.toString()}
                         />
@@ -61,4 +61,4 @@ class About extends Component {
     }     
 
 
-export default About;
+export default connect(mapStateToProps)(About);
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,16 +1,8 @@
 import React, { Component } from 'react';
 import { Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
-import { CAMPSITES } from '../shared/campsites';
 
 class Contact extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES
-        };
-    }
 
     static navigationOptions = {
         title: 'Contact Us'
